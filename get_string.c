e68b87fbb53f00a83cb91e60e0951d17758c43ca
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-04-24 15:36:40 -0700

    Updated get_string

--- get_string.c
+++ get_string.c
@@ -12,14 +12,15 @@ int get_string(char *string)
 	int i = 0;
 	int count = 0;
 
-	if (string)
+	if (string == NULL)
 	{
-		while (string[i] != '\0')
-		{
-			__putchar(string[i]);
-			count += 1;
-			i++;
-		}
+		return (-1);
+	}
+	while (string[i] != '\0')
+	{
+		__putchar(string[i]);
+		count += 1;
+		i++;
 	}
 	return (count);
 }
