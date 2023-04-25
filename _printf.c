f5ce11f8dfe1dadf49080ffd00068c40035420af
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-04-25 13:13:40 -0700

    Updated printf: betty line error

--- _printf.c
+++ _printf.c
@@ -12,44 +12,28 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
-	if (format == NULL)
-	{
+	if (!format)
 		return (-1);
-	}
 	for (j = 0; format[j] != '\0'; j++)
 	{
 		if (format[j] != '%')
-		{
 			__putchar(format[j]);
-			value += 1;
-		}
 		else if (format[j + 1] == 'c')
-		{
 			__putchar(va_arg(args, int));
-			j++;
-			value += 1;
-		}
+			j++
 		else if (format[j + 1] == 's')
-		{
 			r_val = get_string(va_arg(args, char *));
 			j++;
 			value += (r_val - 1);
-			value += 1;
-		}
 		else if (format[j + 1] == '%')
-		{
 			__putchar('%');
 			j++;
-			value += 1;
-		}
 		else if (format[j + 1] == 'i' || format[j + 1] == 'd')
 		{
 			r_val = print_decimal(va_arg(args, int));
 			j++;
 			if (r_val == 0)
-			{
 				int_count++;
-			}
 			else
 			{
 				while (r_val != 0)
@@ -59,6 +43,8 @@ int _printf(const char *format, ...)
 				}
 			}
 		}
+		value += 1;
+
 	}
 	va_end(args);
 	return (value + int_count);
