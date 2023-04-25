5ef66c62411df067b51a9378e55e1dbd44a2be55
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-04-25 13:36:01 -0700

    Updated printf: correct indentation for code function

--- _printf.c
+++ _printf.c
@@ -17,21 +17,29 @@ int _printf(const char *format, ...)
 	for (j = 0; format[j] != '\0'; j++)
 	{
 		if (format[j] != '%')
+		{
 			__putchar(format[j]);
 			value += 1;
+		}
 		else if (format[j + 1] == 'c')
+		{
 			__putchar(va_arg(args, int));
 			j++;
 			value += 1;
+		}
 		else if (format[j + 1] == 's')
+		{
 			r_val = get_string(va_arg(args, char *));
 			j++;
 			value += (r_val - 1);
 			value += 1;
+		}
 		else if (format[j + 1] == '%')
+		{
 			__putchar('%');
 			j++;
 			value += 1;
+		}
 		else if (format[j + 1] == 'i' || format[j + 1] == 'd')
 		{
 			r_val = print_decimal(va_arg(args, int));
@@ -41,8 +49,10 @@ int _printf(const char *format, ...)
 			else
 			{
 				while (r_val != 0)
+				{
 					int_count++;
 					r_val /= 10;
+				}
 			}
 		}
 	}
