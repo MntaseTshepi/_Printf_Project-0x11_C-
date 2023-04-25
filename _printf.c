6d9046521cb045ce6eaaf9f15d51f3620b2e089c
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-04-25 13:25:20 -0700

    Updated printf: compiler error

--- _printf.c
+++ _printf.c
@@ -18,16 +18,20 @@ int _printf(const char *format, ...)
 	{
 		if (format[j] != '%')
 			__putchar(format[j]);
+			value += 1;
 		else if (format[j + 1] == 'c')
 			__putchar(va_arg(args, int));
-			j++
+			j++;
+			value += 1;
 		else if (format[j + 1] == 's')
 			r_val = get_string(va_arg(args, char *));
 			j++;
 			value += (r_val - 1);
+			value += 1;
 		else if (format[j + 1] == '%')
 			__putchar('%');
 			j++;
+			value += 1;
 		else if (format[j + 1] == 'i' || format[j + 1] == 'd')
 		{
 			r_val = print_decimal(va_arg(args, int));
@@ -37,14 +41,10 @@ int _printf(const char *format, ...)
 			else
 			{
 				while (r_val != 0)
-				{
 					int_count++;
 					r_val /= 10;
-				}
 			}
 		}
-		value += 1;
-
 	}
 	va_end(args);
 	return (value + int_count);
