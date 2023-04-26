ff3d78db7dafde9655b59a69afbba7dc37ce44b5
MDIMACat <romanmdima@gmail.com>
2023-04-26 16:32:05 -0700

    Updated printf function: updated get_string.c

--- get_string.c
+++ get_string.c
@@ -10,22 +10,15 @@
 int get_string(char *string)
 {
 	int i = 0;
-	int count = 0;
 
 	if (string == NULL)
 	{
 		string = "(null)";
-		return (-1);
 	}
-	else if (string)
+	while (string[i] != '\0')
 	{
-
-		while (string[i] != '\0')
-		{
-			__putchar(string[i]);
-			count += 1;
-			i++;
-		}
+		__putchar(string[i]);
+		i++;
 	}
-	return (count);
+	return (i);
 }
