0fd1f8684de1807ee8369ecf129e3a22372201e0
MDIMACat <romanmdima@gmail.com>
2023-04-26 14:45:43 -0700

    Updated printf project: Updated functions list

--- functions.c
+++ functions.c
@@ -10,3 +10,28 @@ int print_percentage(void)
 	__putchar('%');
 	return (1);
 }
+
+/**
+ * print_args - Function prints arguments
+ * @format: Format string
+ * @args: Args list
+ * Return: arguements
+ */
+
+int print_args(const char *format, va_list args)
+{
+	switch (*format)
+	{
+		case 'c':
+			return (__putchar(va_arg(args, int)));
+		case 's':
+			return (get_string(va_arg(args, char *)));
+		case 'd':
+		case 'i':
+			return (print_decimal(va_arg(args, int)));
+		case '%':
+			return (print_percentage());
+		default:
+			return (__putchar('%') + __putchar(*format));
+	}
+}
