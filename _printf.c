ad94b3ad6e4bad66c8ea716a8583c373396430fb
MntaseTshepi <maxwellmoilwe@hotmail.com>
2023-04-26 11:26:09 -0700

    Updated printf : %i and %d error fix

--- _printf.c
+++ _printf.c
@@ -36,7 +36,7 @@ int _printf(const char *format, ...)
 			value += get_string(s);
 			j++;
 		}
-		else if (format[j] == '%' && format[j + 1] != '%')
+		else if (format[j + 1] == '%')
 		{
 			value += __putchar('%');
 			j++;
